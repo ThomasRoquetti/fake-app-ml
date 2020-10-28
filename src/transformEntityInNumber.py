a86edc1236961a6b50234a0d56c6ece7a2922907
ThomasBorges <thomas.mr.borges@gmail.com>
2020-10-28 19:35:28 -0300

    true processed

--- src/transformEntityInNumber.py
+++ src/transformEntityInNumber.py
@@ -4,10 +4,11 @@ import numpy as np
 
 header = ['indice','person','organization','event']
 
-fakeEntity = pd.read_csv('fake_entity_gc.csv')
+fakeEntity = pd.read_csv('true_entity_gc.csv')
 fakeEntity.drop(["representative_name"], axis=1, inplace=True)
 fakeEntityMean = fakeEntity.groupby(['indice','entity_type']).mean()
 fakeEntityMean = fakeEntityMean.pivot_table(values='entity_salience', index='indice', columns='entity_type', aggfunc='first')
+fakeEntityMean.drop(["ADDRESS","CONSUMER_GOOD",'OTHER','PHONE_NUMBER','DATE','NUMBER','PRICE','WORK_OF_ART'],axis=1,inplace=True)
 print(fakeEntityMean)
 
-fakeEntityMean.to_csv('fake-entity-processed.csv')
+fakeEntityMean.to_csv('true-entity-processed.csv')
