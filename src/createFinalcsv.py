579eac6aec4e090fd033aa361a87f03d4c49afee
ThomasBorges <thomas.mr.borges@gmail.com>
2020-10-28 22:34:06 -0300

    finished create final

--- src/createFinalcsv.py
+++ src/createFinalcsv.py
@@ -21,15 +21,52 @@ import pandas as pd
 fakeMeta = pd.read_csv("fake-meta-corpus.csv") 
 fakeMeta.drop(["author", "link", "date of publication", "number of links inside the news", "emotiveness", "diversity"], axis=1, inplace=True)
 fakeMeta.set_index('indice', inplace=True)
-print(fakeMeta)
 
-fakeEntity = pd.read_csv("fake_entity_gc.csv")
+fakeEntity = pd.read_csv("fake-entity-processed.csv")
 fakeEntity.set_index("indice", inplace=True)
-print(fakeEntity)
 
 fakeSentiment = pd.read_csv("fake_sentiment_gc.csv")
 fakeSentiment.set_index("indice", inplace=True)
-print(fakeSentiment)
 
 fakeMerged = fakeMeta.merge(fakeSentiment, left_index=True, right_index=True)
-print(fakeMerged)
+fakeMerged = fakeMerged.merge(fakeEntity, left_index=True, right_index=True)
+
+fakeMerged = fakeMerged.drop(fakeMerged.index[[0,3,16,20,23,37,39,90]])
+
+# i=1
+# for index, row in fakeMerged.iterrows():
+#     if int(index) != int(i):
+#         print(i)
+#         i+=1
+#     i+=1
+# print("End fake\n")
+# fakeMerged.reset_index(drop=True, inplace=True)
+#print(fakeMerged)
+
+# Merge toguether csv for true 
+trueMeta = pd.read_csv("true-meta-corpus.csv") 
+trueMeta.drop(["author", "link", "date of publication", "number of links inside the news", "emotiveness", "diversity"], axis=1, inplace=True)
+trueMeta.set_index('indice', inplace=True)
+
+trueEntity = pd.read_csv("true-entity-processed.csv")
+trueEntity.set_index("indice", inplace=True)
+
+trueSentiment = pd.read_csv("true_sentiment_gc.csv")
+trueSentiment.set_index("indice", inplace=True)
+
+trueMerged = trueMeta.merge(trueSentiment, left_index=True, right_index=True)
+trueMerged = trueMerged.merge(trueEntity, left_index=True, right_index=True)
+
+# i=1
+# for index, row in trueMerged.iterrows():
+#     if int(index) != int(i):
+#         print(i)
+#         i+=1
+#     i+=1
+
+trueMerged.reset_index(drop=True, inplace=True)
+#print(trueMerged)
+
+merged = pd.concat([fakeMerged, trueMerged])
+merged.to_csv("finalCSVforTraining.csv")
+
